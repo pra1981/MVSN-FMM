5b0c9df4fc2cc6ad6cb31e4094bc5b667f1307cd
carter-allen <carter.allen12@gmail.com>
2019-05-13 09:18:55 -0400

    first commit

---
+++ simulation/tables.R
@@ -0,0 +1,16 @@
+# Analyze MCMC draws
+
+library(tidyverse)
+
+# Load draws
+
+load("mcmc_draws_2019-03-26/BETA")
+load("mcmc_draws_2019-03-26/beta_true")
+load("mcmc_draws_2019-03-26/DELTA")
+load("mcmc_draws_2019-03-26/delta_true")
+load("mcmc_draws_2019-03-26/PI")
+load("mcmc_draws_2019-03-26/PSI")
+load("mcmc_draws_2019-03-26/psi_true")
+load("mcmc_draws_2019-03-26/SIGMA")
+load("mcmc_draws_2019-03-26/sig2_true")
+load("mcmc_draws_2019-03-26/Z")
